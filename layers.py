07f5eecbc149fcc9e4efa59c5376596518d21d54
Psiphen <45835237+awesomepgm@users.noreply.github.com>
2024-04-19 16:31:51 -0700

    Added linearnet

--- layers.py
+++ layers.py
@@ -3,6 +3,30 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
+class LinearNet(nn.Module):
+    def __init__(self, layer_sizes=[], leaky_relu_alpha = 0.2, dropout_p = 0, final_linear = False):
+        super(LinearNet, self).__init__()
+        self.final_linear = final_linear
+        self.leaky_relu_alpha = leaky_relu_alpha
+        self.dropout = nn.Dropout(p=dropout_p)
+        
+        self.net = nn.ModuleList()
+        for i in range(len(layer_sizes) - 1):
+            linear = nn.Linear(layer_sizes[i], layer_sizes[i + 1])
+            
+            self.net.append(linear)
+        
+    def forward(self, x):
+        for i in range(len(self.net)):
+            x = self.net[i](x)
+            if i != len(self.net) - 1 or not self.final_linear:
+                x = F.leaky_relu(x, negative_slope=self.leaky_relu_alpha)
+                
+            x = self.dropout(x)
+        
+        return x        
+
 # Kind of useless since it would be slow to loop this but it's a good example of how to implement it
 class Attention(nn.Module):
     def __init__(self, activation_function = torch.softmax):
@@ -16,27 +40,31 @@ class Attention(nn.Module):
         return torch.matmul(logits, V)
 
 class MAB(nn.Module):
-    def __init__(self, embed_dim, num_heads, activation_function = torch.softmax, layer_norm = True, dropout = 0.1):
+    def __init__(self, embed_dim, num_heads, ff_layers = None, final_linear = True, activation_function = torch.softmax, layer_norm = False, dropout = 0.1):
         assert embed_dim % num_heads == 0, "Embedding dimension must be divisible by number of heads"
         
         super(MAB, self).__init__()
         self.activation_function = activation_function
         
-        self.attention = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, dropout=dropout)
+        self.attention = nn.MultiheadAttention(embed_dim=embed_dim, num_heads=num_heads, batch_first=True)
         self.layer_norm = nn.LayerNorm(embed_dim) if layer_norm else None
         
-        self.feedforward = nn.LinearNet() # FIXME: THIS IS A PLACEHOLDER FOR THE FEEDFORWARD LAYER
+        self.feedforward = LinearNet([embed_dim] + ff_layers + [embed_dim], final_linear=final_linear) 
+        self.dropout = nn.Dropout(p=dropout)
     
     def forward(self, X, Y):
-        attention = self.attention(X, Y, Y)
+        attention = self.attention(X, Y, Y, need_weights=False)[0]
+        H = X + attention
         
         if self.layer_norm:
-            H = self.layer_norm(X + attention)
-            
+            H = self.layer_norm(H)
+        H = self.dropout(H)
+        
         mab_out = H + self.feedforward(H)
         
         if self.layer_norm:
-            return self.layer_norm(mab_out)
+            mab_out = self.layer_norm(mab_out)
+        mab_out = self.dropout(mab_out)
         
         return mab_out
     
@@ -45,7 +73,6 @@ class PMA(nn.Module):
         super(PMA, self).__init__()
         self.seed_count = seed_count
         
-        self.feedforward = nn.LinearNet() # FIXME: THIS IS A PLACEHOLDER FOR THE FEEDFORWARD LAYER
         self.mab = MAB(embed_dims, **mab_args)
         self.S = nn.Parameter(torch.empty(1, seed_count, embed_dims))
         nn.init.xavier_uniform_(self.S)
@@ -77,3 +104,16 @@ class IPAB(nn.Module):
     def forward(self, X, Z):
         Z = self.mab1(Z.unsqueeze(1), X)
         return self.mab2(X, Z), Z.squeeze(1)
+
+if __name__ == "__main__":
+    # Test the layers
+    net = LinearNet([10, 20, 30, 1])
+    x = torch.randn(16,10)
+    # print(net(x), net(x).shape)
+    
+    torch.manual_seed(0)
+    ipab = IPAB(10, num_heads=2, ff_layers = [10,20,10])
+    x = torch.ones(16, 30, 10)
+    z = torch.ones(16, 10)
+    print(ipab(x, z))
+    
